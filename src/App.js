99df5cbf2f4ec19f9912290e7b1aa5ee4d9138eb
John <jddhalogamer@gmail.com>
2021-10-05 13:39:54 -0500

    rewrote state to be in an object, and used object destructering.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import './App.css';
 
 const App = () => {
 
-  const [ left, setLeft ] = useState(0);
-  const [ right, setRight ] = useState(0);
+  const [ clicks, setClicks ] = useState({
+    left: 0, right: 0
+  });
+
+  const handleLeftClick = () => 
+    setClicks({ ...clicks, left: clicks.left + 1})
  
+  const handleRightClick = () => 
+    setClicks({ ...clicks, right: clicks.right + 1})
+
   return (
     <div className="App">
-      {left}
-      <button onClick={() => setLeft(left + 1)}> 
+      {clicks.left}
+      <button onClick={() => handleLeftClick()}> 
         left
       </button>
-      <button onClick={() => setRight(right + 1)}>
+      <button onClick={() => handleRightClick()}> 
         right
       </button>
-      {right}
+      {clicks.right}
     </div>
   );
 }
